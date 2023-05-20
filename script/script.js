cab0f87d6967130634435004812fa4ac103a3699
jdwcg <bigcloudjo@gmail.com>
2023-05-20 21:04:47 +0900

    v

--- script/script.js
+++ script/script.js
@@ -20,40 +20,49 @@ $(function () {
     $(".galleryBtn").css("background", "white");
   });
   //   메뉴 AAAAA
-  $(".nav ul li").mouseenter(function () {
-    $(".nav ul li ul").stop().slideDown();
-  });
-  $(".nav").mouseleave(function () {
-    $(".nav ul li ul").stop().slideUp();
-  });
+  // $(".nav ul li").mouseenter(function () {
+  //   $(".nav ul li ul").stop().slideDown();
+  // });
+  // $(".nav").mouseleave(function () {
+  //   $(".nav ul li ul").stop().slideUp();
+  // });
+
   //   메뉴 BBBBB
-  //   $(".nav ul li a").mouseenter(function () {
-  //     $(this).siblings().stop().slideDown();
-  //     console.log(0);
-  //   });
-  //   $(".nav ul li a").mouseleave(function () {
-  //     $(this).siblings().stop().slideUp();
-  //   });
+  // $(".nav ul li a").mouseenter(function () {
+  //   $(this).siblings().stop().slideDown();
+  //   console.log(0);
+  // });
+  // $(".nav ul li a").mouseleave(function () {
+  //   $(this).siblings().stop().slideUp();
+  // });
+
+  //   메뉴 CCCCC
+  $(".nav ul li").mouseenter(function () {
+    $(this).children("ul").stop().slideDown(); /* fadeIn or slideDown */
+  });
+  $(".nav ul li").mouseleave(function () {
+    $(this).children("ul").stop().slideUp(); /* fadeIn or slideUp */
+  });
 
   // ↓↓↓↓ 슬라이더 가로
-  //   setInterval(function () {
-  //     $(".slider").animate({ "margin-left": "-100%" }, function () {
-  //       $(".img_wrap:first").appendTo(".slider");
-  //       $(".slider").css({ "margin-left": "0" });
-  //     });
-  //   }, 3000);
+  // setInterval(function () {
+  //   $(".slider").animate({ "margin-left": "-100%" }, function () {
+  //     $(".img_wrap:first").appendTo(".slider");
+  //     $(".slider").css({ "margin-left": "0" });
+  //   });
+  // }, 3000);
 
   // ↓↓↓↓ 슬라이더 세로
-  setInterval(function () {
-    $(".slider").animate({ "margin-top": "-300px" }, function () {
-      $(".img_wrap:first").appendTo(".slider");
-      $(".slider").css({ "margin-top": "0" });
-    });
-  }, 3000);
-  //   ↓↓↓↓ 슬라이더 제자리
-  //   $(".img_wrap:gt(0)").hide();
-  //   setInterval(function () {
-  //     $(".img_wrap:first").fadeOut(1500).next().fadeIn(1500);
+  // setInterval(function () {
+  //   $(".slider").animate({ "margin-top": "-300px" }, function () {
   //     $(".img_wrap:first").appendTo(".slider");
-  //   }, 3000);
+  //     $(".slider").css({ "margin-top": "0" });
+  //   });
+  // }, 3000);
+  //   ↓↓↓↓ 슬라이더 제자리
+  // $(".img_wrap:gt(0)").hide();
+  // setInterval(function () {
+  //   $(".img_wrap:first").fadeOut(1500).next().fadeIn(1500);
+  //   $(".img_wrap:first").appendTo(".slider");
+  // }, 3000);
 });
